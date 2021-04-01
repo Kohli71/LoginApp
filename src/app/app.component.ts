5bd4f4299f27fc379580ae572c6adcb5c2682ea0
Kranthi0307 <you@example.com>
2021-04-01 15:22:03 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LoginApp';
+  emailid: any;
+  formdata: any;
+  
+  ngOnInit(){
+    this.formdata = new FormGroup({
+      emailid: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("[^ @]*@[^ @]*")
+      ])),
+      password: new FormControl("")
+    });
+  }
+
+  onClickSubmit(data: { emailid: any; }){
+    this.emailid = data.emailid;
+  }
 }
